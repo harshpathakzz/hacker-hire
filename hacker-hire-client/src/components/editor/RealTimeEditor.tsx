97c0f04619fc8b3eb926f963f358677d20226d19
harshpathakzz <harshpathak2017@gmail.com>
2024-04-27 10:44:57 +0530

    udated

--- hacker-hire-client/src/components/editor/RealTimeEditor.tsx
+++ hacker-hire-client/src/components/editor/RealTimeEditor.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
-
 import firebase from "firebase/app";
 import "firebase/database";
 import { fromMonaco } from "@hackerrank/firepad";
@@ -21,54 +20,59 @@ interface RealtimeEditorProps {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 export default function RealtimeEditor(params: RealtimeEditorProps) {
   console.log(params.roomId);
+
   const { sandpack } = useSandpack();
   const { code, updateCode } = useActiveCode();
-  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+  const [editor, setEditor] =
+    useState<monaco.editor.IStandaloneCodeEditor | null>(null);
   const [editorLoaded, setEditorLoaded] = useState(false);
   const [userName, setUserName] = useState<string>("");
+
   function handleEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor) {
-    editorRef.current = editor;
+    setEditor(editor);
     console.log("Editor ref changes");
     setEditorLoaded(true);
   }
 
   useEffect(() => {
-    if (!editorLoaded) {
+    if (!editorLoaded || !editor) {
       return;
     }
 
     const activeFilePath = sandpack.activeFile;
-    const sanitizedActiveFile = sandpack.activeFile.replace(/[.#$\\[\]]/g, "");
+    const sanitizedActiveFile = sandpack.activeFile.replace(
+      /[\\.#$\\\\[\\\]]/g,
+      ""
+    );
     const dbRef = firebase
       .database()
       .ref()
       .child(`${params.roomId}/${sanitizedActiveFile}`);
 
     let name = userName;
-
     if (!userName) {
       const name = prompt("Enter your Name:") ?? "";
       setUserName(name);
     }
 
-    if (!editorRef.current) {
-      return;
-    }
-    console.log(editorRef.current);
-    const firepad = fromMonaco(dbRef, editorRef.current);
+    console.log(editor);
+
+    const firepad = fromMonaco(dbRef, editor);
     firepad.setUserName(name);
+
     return () => {
       firepad.dispose();
     };
-  }, [editorLoaded, params.roomId, sandpack.activeFile, userName]);
+  }, [editorLoaded, params.roomId, sandpack.activeFile, userName, editor]);
 
   return (
     <div>
       <SandpackStack style={{ height: "95vh", margin: 0 }}>
         <FileTabs closableTabs={true} />
-
         <div style={{ flex: 1, paddingTop: 8, background: "#1e1e1e" }}>
           <Editor
             width="100%"
