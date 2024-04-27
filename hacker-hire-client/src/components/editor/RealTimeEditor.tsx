4602c445e2f52fd70906cbfb473f5fe6a7f7143f
harshpathakzz <harshpathak2017@gmail.com>
2024-04-27 10:15:56 +0530

    updated

--- hacker-hire-client/src/components/editor/RealTimeEditor.tsx
+++ hacker-hire-client/src/components/editor/RealTimeEditor.tsx
@@ -27,29 +27,42 @@ export default function RealtimeEditor(params: RealtimeEditorProps) {
   const { code, updateCode } = useActiveCode();
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
   const [editorLoaded, setEditorLoaded] = useState(false);
-
+  const [userName, setUserName] = useState<string>("");
   function handleEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor) {
     editorRef.current = editor;
+    console.log("Editor ref changes");
     setEditorLoaded(true);
   }
 
   useEffect(() => {
-    if (editorLoaded) {
-      const activeFilePath = sandpack.activeFile;
-      const sanitizedActiveFile = sandpack.activeFile.replace(/[.#$\[\]]/g, "");
-      const dbRef = firebase
-        .database()
-        .ref()
-        .child(`${params.roomId}/${sanitizedActiveFile}`);
-
-      const name = prompt("Enter your Name :");
-
-      if (!!editorRef.current && !!name) {
-        const firepad = fromMonaco(dbRef, editorRef.current);
-        firepad.setUserName(name);
-      }
+    if (!editorLoaded) {
+      return;
     }
-  }, [editorLoaded, params.roomId, sandpack.activeFile]);
+
+    const activeFilePath = sandpack.activeFile;
+    const sanitizedActiveFile = sandpack.activeFile.replace(/[.#$\\[\]]/g, "");
+    const dbRef = firebase
+      .database()
+      .ref()
+      .child(`${params.roomId}/${sanitizedActiveFile}`);
+
+    let name = userName;
+
+    if (!userName) {
+      const name = prompt("Enter your Name:") ?? "";
+      setUserName(name);
+    }
+
+    if (!editorRef.current) {
+      return;
+    }
+    console.log(editorRef.current);
+    const firepad = fromMonaco(dbRef, editorRef.current);
+    firepad.setUserName(name);
+    return () => {
+      firepad.dispose();
+    };
+  }, [editorLoaded, params.roomId, sandpack.activeFile, userName]);
 
   return (
     <div>
