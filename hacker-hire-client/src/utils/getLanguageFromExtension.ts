959859a28a4852f6e1a0758fb66288d1f9820616
harshpathakzz <harshpathak2017@gmail.com>
2024-04-28 11:09:44 +0530

    added more languages

--- hacker-hire-client/src/utils/getLanguageFromExtension.ts
+++ hacker-hire-client/src/utils/getLanguageFromExtension.ts
@@ -1,52 +1,59 @@
 export const getLanguageFromExtension = (fileExtension: string) => {
-    switch (fileExtension) {
-      case "js":
-      case "jsx":
-        return "javascript";
-      case "ts":
-      case "tsx":
-        return "typescript";
-      case "css":
-        return "css";
-      case "html":
-        return "html";
-      case "md":
-        return "markdown";
-      case "py":
-        return "python";
-      case "java":
-        return "java";
-      case "cpp":
-      case "cc":
-      case "cxx":
-        return "cpp";
-      case "c":
-        return "c";
-      case "cs":
-        return "csharp";
-      case "go":
-        return "go";
-      case "rb":
-        return "ruby";
-      case "php":
-        return "php";
-      case "swift":
-        return "swift";
-      case "m":
-        return "objective-c";
-      case "sh":
-        return "bash";
-      case "sql":
-        return "sql";
-      case "xml":
-        return "xml";
-      case "yaml":
-      case "yml":
-        return "yaml";
-      case "json":
-        return "json";
-      default:
-        return "javascript";
-    }
-  };
-  
+  switch (fileExtension) {
+    case "js":
+      return "javascript";
+    case "jsx":
+      return "javascriptreact";
+    case "vue":
+      return "vue";
+    case "ts":
+      return "typescript";
+    case "tsx":
+      return "typescriptreact";
+    case "css":
+      return "css";
+    case "html":
+      return "html";
+    case "md":
+      return "markdown";
+    case "py":
+      return "python";
+    case "java":
+      return "java";
+    case "cpp":
+    case "cc":
+    case "cxx":
+      return "cpp";
+    case "c":
+      return "c";
+    case "cs":
+      return "csharp";
+    case "go":
+      return "go";
+    case "rb":
+      return "ruby";
+    case "php":
+      return "php";
+    case "swift":
+      return "swift";
+    case "m":
+      return "objective-c";
+    case "sh":
+      return "bash";
+    case "sql":
+      return "sql";
+    case "xml":
+      return "xml";
+    case "yaml":
+    case "yml":
+      return "yaml";
+    case "json":
+      return "json";
+    case "astro":
+      return "plaintext";
+    case "scss":
+      return "scss";
+    default:
+      return "javascript";
+  }
+};
